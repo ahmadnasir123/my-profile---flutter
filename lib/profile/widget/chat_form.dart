8b7b546a50e2e4e44b51bd059804fc24451facb5
ahmadnasir123 <nasirahmad240502@gmail.com>
2023-12-07 12:43:06 +0700

    update chat_form.dart

--- lib/profile/widget/chat_form.dart
+++ lib/profile/widget/chat_form.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class ChatForm extends StatefulWidget {
@@ -24,49 +22,51 @@ class _ChatFormState extends State<ChatForm> {
       appBar: AppBar(
         title: Text('Halaman Chat'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        child: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                controller: myController,
-                decoration: InputDecoration(hintText: 'Ketikan Nama Anda'),
-                autofocus: true,
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 68, 255, 74),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(15.0),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: myController,
+                  decoration: InputDecoration(hintText: 'Ketikan Nama Anda'),
+                  autofocus: true,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 68, 255, 74),
+                      ),
+                      child: Text('Kirim'),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: Text(
+                                    '${myController.text.toUpperCase()}, Pesan anda berhasil dikirim'),
+                              );
+                            });
+                      },
                     ),
-                    child: Text('Kirim'),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              content: Text(
-                                  '${myController.text.toUpperCase()}, Pesan anda berhasil dikirim'),
-                            );
-                          });
-                    },
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  ElevatedButton(
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                    child: Text('Clear'),
-                    onPressed: () {
-                      myController.clear();
-                    },
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    ElevatedButton(
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      child: Text('Clear'),
+                      onPressed: () {
+                        myController.clear();
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
